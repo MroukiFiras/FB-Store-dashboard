901f7b1a1c1b22a93a64eb6aa20b037fd91a6e8a
MroukiFiras <114618223+MroukiFiras@users.noreply.github.com>
2024-01-12 20:28:14 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:'admin', component:AdminLayoutComponent},
+  {path: '', redirectTo: '/admin', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
